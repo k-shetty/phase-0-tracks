29085cee17c4e61b35125dd0000049fa3e1ed7f3
Karan Shetty <kshetty614@gmail.com>
2016-11-17 23:30:33 -0500

    updated secret_agents.rb.

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -17,7 +17,7 @@ def encrypt(password)
 		end
 	index += 1
 	end
-#puts password
+#return password
 end
 
 #Decrypt
@@ -36,7 +36,7 @@ def decrypt(password)
 		password[index] = ALPHABET[ALPHABET.index(password[index])-1]
 		index += 1
 	end
-#puts password
+#return password
 end
 
 #encrypt("abc") #should return "bcd"
@@ -52,37 +52,37 @@ end
 #Theoretically, using a method as an argument for another method should work, as long as the return value of the
 #nested method is vaild as a input for the parent method. In our case, the individual methods funtioned as expected
 #but we ran into a NoMethodError (lines 8 and 31 ). We believe this happened as a result of the reutrn value for our 
-#nested method not being accepted as an apprpriate value for the parent method.
+#nested method not being accepted as an appropriate value for the parent method.
 
 #DRIVER CODE
 #Prompt user whether or not to encrypt or decrypt 
-puts "Do you want to encrypt or decrypt you password?"
-answer = gets.chomp.downcase
-#Prompt user for password to encrypt or decrypt
+	puts "Do you want to encrypt or decrypt you password?"
+	answer = gets.chomp.downcase
+	#Prompt user for password to encrypt or decrypt
 
-valid_response = false
+	valid_response = false
 
-until valid_response
-	if answer == "encrypt"
-		puts "What password do you want to encrypt?"
-		password = gets.chomp
-		puts encrypt(password)
-		valid_response = true
+	until valid_response
+		if answer == "encrypt"
+			puts "What password do you want to encrypt?"
+			password = gets.chomp
+			puts encrypt(password)
+			valid_response = true
 
-	elsif answer == "decrypt"
-		puts "What password do you want to decrypt?"
-		password = gets.chomp
-		puts decrypt(password)
-		valid_response = true
+		elsif answer == "decrypt"
+			puts "What password do you want to decrypt?"
+			password = gets.chomp
+			puts decrypt(password)
+			valid_response = true
 
-	else
-		puts "Inavlid response. Encrypt or decrypt?"
-		answer = gets.chomp.downcase
-		valid_response = false
+		else
+			puts "Inavlid response. Encrypt or decrypt?"
+			answer = gets.chomp.downcase
+			valid_response = false
+		end
 	end
-end
 
-p "Thank you using our crypto prgram. Your new password is #{password}. Exiting now"
-		
+	p "Thank you using our crypto prgram. Your new password is #{password}. Exiting now"
+			
 
 #Run correct method, print new password to screen, and then close program.
