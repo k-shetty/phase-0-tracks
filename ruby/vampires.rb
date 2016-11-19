b1be91fa21b48e381a0f4d35b60867bf1210e670
Karan Shetty <kshetty614@gmail.com>
2016-11-18 21:59:54 -0500

    Added interview questions to vampire.rb.

---
+++ ruby/vampires.rb
@@ -0,0 +1,18 @@
+#Coding program to gather informmation from user
+
+puts "What is your name?"
+e_name = gets.chomp
+
+puts "How old are you?"
+e_age = gets.chomp
+
+puts "What year were you born"
+e_birthyear = gets.chomp
+
+puts "Would you like us to order you some complimentary garlic bread from our cafeteria?"
+e_bread = gets.chomp
+
+puts "Would you like to enroll in the company's health insurance?"
+e_insurance = gets.chomp
+
+
