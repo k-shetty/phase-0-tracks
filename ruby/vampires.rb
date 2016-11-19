5f36a0f5669e4194f76715a66640fdf08483320e
Karan Shetty <kshetty614@gmail.com>
2016-11-19 00:23:08 -0500

    Added code for release 5 in vampires.rb.

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -51,6 +51,8 @@ while index < TOTALAPPLICANTS
 index += 1
 end
 
+p "Actually, never mind! What do these questions have to do with anything? Let's all be friends."
+
 		
 =begin (release 4)
 add an until loop within total applicants loop asking for allergie
