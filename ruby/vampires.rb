bd51e1960dc99f39d714bad0bfbd9a2371d1da56
Karan Shetty <kshetty614@gmail.com>
2016-11-19 02:17:30 -0500

    Modified some conditional logic in vampires.rb.

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -3,8 +3,6 @@ puts "what is the total number of applicants today"
 TOTALAPPLICANTS = gets.chomp.to_i
 
 
-
-
 #Conditional Logic
 index = 0
 
@@ -35,7 +33,7 @@ while index < TOTALAPPLICANTS
 
 			if (e_allergy == "sunshine")
 				puts "Probably a vampire"
-			elsif (e_age == (2016 - e_birthyear)) && ((e_bread == "yes") && (e_insurance == "yes") && (e_name == "drake cula") || (e_name == "tu fang"))
+			elsif ((e_name == "drake cula") || (e_name == "tu fang"))
 				puts "Definitely a Vampire."
 			elsif (e_age != (2016 - e_birthyear)) && (e_bread == "no") && (e_insurance == "no")
 				puts "Almost certainly a vampire."
@@ -43,8 +41,6 @@ while index < TOTALAPPLICANTS
 				puts "Probably a vampire."
 			elsif (e_age == (2016 - e_birthyear)) && ((e_bread == "yes") || (e_insurance == "yes"))
 				puts "Probably not a vampire."
-			#elsif (e_allergy == "sunshine") 
-			#	puts "Probably not a vampire."
 			else
 				puts "Results inconclusive."
 			end
@@ -54,49 +50,51 @@ end
 p "Actually, never mind! What do these questions have to do with anything? Let's all be friends."
 
 		
-=begin (release 4)
-add an until loop within total applicants loop asking for allergie
+#  (release 4)
+# add an until loop within total applicants loop asking for allergies
 
-end
+# (e_age = (2016 - e_birthyear)) && ((e_bread == "yes") && (e_insurance == "yes") && 
 
-
-=begin (Repeat process for number of applicants)
-
-while loop
-set index = 0
-while index < number of applicants 
-
-run logic, print result for employee, move to next employee
+# end
 
 
+# =begin (Repeat process for number of applicants)
+
+# while loop
+# set index = 0
+# while index < number of applicants 
+
+# run logic, print result for employee, move to next employee
 
 
 
 
-=begin (conditonal logic)
+
+
+# =begin (conditonal logic)
 	
-If the employee got their age right, and is willing to eat garlic bread or sign up for insurance, the result is “Probably not a vampire.”
+# If the employee got their age right, and is willing to eat garlic bread or sign up for insurance, the result is “Probably not a vampire.”
 
-(e_age == (2016-e_birthyear)) && ((e_bread == "yes") || (e_insurance == "yes"))
-puts "Probably not a vampire."
+# (e_age == (2016-e_birthyear)) && ((e_bread == "yes") || (e_insurance == "yes"))
+# puts "Probably not a vampire."
 
-If the employee got their age wrong, and hates garlic bread or waives insurance, the result is “Probably a vampire.”
+# If the employee got their age wrong, and hates garlic bread or waives insurance, the result is “Probably a vampire.”
 
-(e_age != (2016- e_birthyear)) && ((e_bread == "no") || (e_insurance == "no"))
-puts "Probably a vampire."
+# (e_age != (2016- e_birthyear)) && ((e_bread == "no") || (e_insurance == "no"))
+# puts "Probably a vampire."
 
-If the employee got their age wrong, hates garlic bread, and doesn’t want insurance, the result is “Almost certainly a vampire.”
+# If the employee got their age wrong, hates garlic bread, and doesn’t want insurance, the result is “Almost certainly a vampire.”
 
-(e_age != (2016- e_birthyear)) && (e_bread == "no") && (e_insurance == "no")
-puts "Almost certainly a vampire."
+# (e_age != (2016- e_birthyear)) && (e_bread == "no") && (e_insurance == "no")
+# puts "Almost certainly a vampire."
 
-Even if the employee is an amazing liar otherwise, anyone going by the name of “Drake Cula” or “Tu Fang” is clearly a vampire, because come on. In that case, you should print “Definitely a vampire.”
-Otherwise, print “Results inconclusive.”
+# Even if the employee is an amazing liar otherwise, anyone going by the name of “Drake Cula” or “Tu Fang” is clearly a vampire, because come on. In that case, you should print “Definitely a vampire.”
+# Otherwise, print “Results inconclusive.”
 
-(e_age == (2016-e_birthyear)) && ((e_bread == "yes") && (e_insurance == "yes") && (e_name == "drake cula") || (e_name == "tu fang"))
-puts "Definitely a Vampire."
+# (e_age == (2016-e_birthyear)) && ((e_bread == "yes") && (e_insurance == "yes") && (e_name == "drake cula") || (e_name == "tu fang"))
+# puts "Definitely a Vampire."
 
-else 
-	puts "Results inconclusive."
+# else 
+# 	puts "Results inconclusive."
 	
-=end
+
