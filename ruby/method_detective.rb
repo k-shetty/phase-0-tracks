952e169c6b89c41ca9922333c8348a033bbd202c
Karan Shetty <kshetty614@gmail.com>
2016-11-22 14:55:44 -0500

    Added comments to method_detective.rb.

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -6,34 +6,44 @@
 
 # "iNvEsTiGaTiOn".<???>
 # => “InVeStIgAtIoN”
+#.swapcase for now
 
 # "zom".<???>
 # => “zoom”
+#.insert
 
 # "enhance".<???>
 # => "    enhance    "
+#.center for now
 
 # "Stop! You’re under arrest!".<???>
 # => "STOP! YOU’RE UNDER ARREST!"
+#.upcase
 
 # "the usual".<???>
 #=> "the usual suspects"
+#.concat for now, .insert as well
 
 # " suspects".<???>
 # => "the usual suspects"
+#.insert or .concat or .prepend or .rjust
 
 # "The case of the disappearing last letter".<???>
 # => "The case of the disappearing last lette"
+#.chop
 
 # "The mystery of the missing first letter".<???>
 # => "he mystery of the missing first letter"
+#.slice
 
 # "Elementary,    my   dear        Watson!".<???>
 # => "Elementary, my dear Watson!"
+#.squeeze
 
 # "z".<???>
 # => 122 
 # (What is the significance of the number 122 in relation to the character z?)
+#.ord
 
 # "How many times does the letter 'a' appear in this string?".<???>
 # => 4
