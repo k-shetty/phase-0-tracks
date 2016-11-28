e28478523e6d47fcc8328b9c82612e10dcd7b8e0
Karan Shetty <kshetty614@gmail.com>
2016-11-27 19:11:51 -0500

    Added method for changing to next letter in alias_manager.rb.

---
+++ ruby/alias_manager.rb
@@ -0,0 +1,34 @@
+#creating algorithm
+#get user input
+# split between first name and last name
+#iterate through each name
+#identify consonants and vowels
+#	-consonants move to next consonant
+#	-vowels move to next vowel
+#Join final result
+#Save results
+name = "Karan Shetty".downcase.split(' ').reverse.
+
+# $vowels = ("aeiou".split(''))
+# $consonants = ("bcdfghjklmnpqrstvwxyz".split(''))
+
+return name
+
+
+# def alias_creation(name)
+	
+# end
+
+
+
+# def next_character(character)
+# 	if $vowels.include?(character)
+# 		p $vowels[($vowels.index(character)+ 1)]
+# 	else
+# 		p $consonants[($consonants.index(character) + 1)]
+# 	end
+# end
+
+# puts next_character("b")
+
+
