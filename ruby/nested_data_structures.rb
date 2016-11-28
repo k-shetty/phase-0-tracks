7fb91f70b6e4d44531b912b9fb38f6c3cd3d0498
Karan Shetty <kshetty614@gmail.com>
2016-11-28 01:29:27 -0500

    Added nested data structure to nested_data_structures.rb.

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,59 @@
+classroom = {
+    desk1: {
+        student_name: 'Chet Manly',
+        subject_textbooks: {
+            Biology: 2,
+            Mathematics: 4
+        },
+        writing_instruments: [
+            "G2 Pilot Pens", 
+            "Mechanical Pencils"
+        ]
+    },
+    
+    desk2: {
+        student_name: 'Rip Riley',
+        subject_textbooks: {
+            Biology: 2,
+            Geography: 2,
+            Physics: 3
+        },
+        writing_instruments: [
+            "Bic Pens", 
+            "Mechanical Pencils",
+            "A compass"
+        ]
+    },
+
+    desk3: {
+        student_name: 'Pam Poovey',
+        subject_textbooks: {
+            Agriculture: 5,
+            Mathematics: 1,
+            Art:2,
+            Chemistry: 3
+        },
+        writing_instruments: [
+            "Uniball Pens", 
+            "Mechanical Pencils",
+            "Oil Pastels"
+        ]
+    },
+
+    desk4: {
+        student_name: 'Ray Gillete',
+        subject_textbooks: {
+            Calligraphy: 2,
+            History: 4,
+            English: 3
+
+        },
+        writing_instruments: [
+            "G2 Pilot Pens", 
+            "Mechanical Pencils",
+            "Fountain Pens"
+        ]
+    },
+    }
+}
+
