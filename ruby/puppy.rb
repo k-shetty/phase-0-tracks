901de073614fe71aa60c70b8f69875f9357803bd
Karan Shetty <kshetty614@gmail.com>
2016-12-01 17:53:12 -0500

    Add class design comments to puppy.rb.

---
+++ ruby/puppy.rb
@@ -0,0 +1,22 @@
+#Class Design for puppies
+
+=begin
+
+SPECIES ------------------------
+Canis familiaris
+
+CHARACTERISTICS ----------------
+Friendly: yes
+Eye count: 2
+Ear Count: 2
+Name: varies
+Color: varies
+# of legs: 4
+
+BEHAVIOR -----------------------
+Jump
+Running
+Sniffing
+Licking
+
+=end
