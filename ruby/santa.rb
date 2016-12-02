cdd97e48dec81c47485f891e72ea50e01ae05809
Karan Shetty <kshetty614@gmail.com>
2016-12-02 13:04:45 -0500

    Add instance methods to Santa class and driver code to test methods in santa.rb.

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,3 +1,21 @@
+#Created Santa class and added three instance methods
 class Santa
 
+	def initialize
+		puts "Initializing Santa instance..."
+	end
+
+	def speak
+		puts "Ho, ho, ho! Haaaapy Holidays!"
+	end
+
+	def eat_milk_and_cookies(type_of_cookie)
+		puts "That was a good #{type_of_cookie} cookie."
+	end
+
 end
+
+#Driver code to test
+santa = Santa.new
+santa.speak
+santa.eat_milk_and_cookies("chocolate chip")
