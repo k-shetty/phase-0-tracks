a3e5f16c6133ff407dcbd06ab6af494ab9d87648
Karan Shetty <kshetty614@gmail.com>
2016-12-02 14:23:40 -0500

    Refactor code using attr_reader and attr_accessor in santa.rb.

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,12 +1,15 @@
 #Created Santa class and added three instance methods
 class Santa
+	attr_reader :age, :ethnicity
+	attr_accessor :gender
 
 	def initialize(gender, ethnicity)
 		#puts "Initializing Santa instance..."
 		@gender = gender
+		@age = 0
 		@ethnicity = ethnicity
 		@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
-		@age = 0
+		
 	end
 
 	def speak
@@ -16,6 +19,31 @@ class Santa
 	def eat_milk_and_cookies(type_of_cookie)
 		puts "That was a good #{type_of_cookie} cookie."
 	end
+
+	def celebrate_birthday
+		@age += 1
+	end
+
+	def get_mad_at(reinder_name)
+		@reindeer_ranking.delete(reinder_name)
+		@reindeer_ranking << reinder_name
+	end
+#Getter Methods
+#commented out getter methods due to attr_reader
+	# def age
+	# 	@age
+	# end 
+
+	# def ethnicity
+	# 	@ethnicity
+	# end
+	
+#Setter Method
+#commented out setter method due to attr_accessor
+	# def gender=(new_gender)
+	# 	@gender = new_gender
+	# end
+
 
 end
 
@@ -38,7 +66,21 @@ end
 # example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
 # example_genders.length.times do |i|
 #   santas << Santa.new(example_genders[i], example_ethnicities[i])
-end
+# end
 
-p santas
+sample_santa = Santa.new("male", "asian")
+sample_santa.gender = "something"
+
+p sample_santa
+p sample_santa.age
+p sample_santa.celebrate_birthday
+p sample_santa.ethnicity
+p sample_santa.age
+
+
+# p santas
+
+
+
+
 
