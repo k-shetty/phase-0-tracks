34cbded1ce3bdb7436ffd68eff32a224df2042cb
Karan Shetty <kshetty614@gmail.com>
2016-12-03 15:44:45 -0500

    Delete psudeo_for_game.md. Add pseudocode to guess_the_word.rb. Add beginning framework for program file and test file in directory.

---
+++ ruby/word-guessing-game/guess_the_word.rb
@@ -0,0 +1,21 @@
+#pseudocode for challenge
+# Take input and pass it through initialize method to start game (this becomes the secret word I guess)
+# 	-input needs to be hidden from player
+# 	-input length determines number of avaiable guesses
+# Player has certain number of tries to guess some letters of input word.
+# 	-method for this takes player input for character (handled by driver code), compares it to secret word
+#	-if player has matching character, then method returns secret word with correctly guessed characters displayed 
+#	 but everything else still hidden.
+#   -if player guess doesn't match, return last correct state of hidden word
+#	-for every guess, one gets removed from total tries
+# Once guesses are over, player has one final chance to guess the word
+# =>-allows player to type out word of they know it
+# Pass message onto player if game is won or lost.
+
+
+class GuessWordGame
+	def intialize()
+		
+	end
+
+end
---
+++ ruby/word-guessing-game/guess_the_word_spec.rb
@@ -0,0 +1,7 @@
+require_relative 'guess_the_word'
+
+describe GuessWordGame do
+	it "" do
+	end
+	
+end
