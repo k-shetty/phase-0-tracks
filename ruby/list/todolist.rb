395ee14421498034421b485dd2f6881b67c52a8f
Karan Shetty <kshetty614@gmail.com>
2016-12-02 21:19:04 -0500

    Add some methods to todolist.rb and add require_relative to todo_list_spec.rb.

--- ruby/list/todolist.rb
+++ ruby/list/todolist.rb
@@ -1,17 +1,35 @@
 class TodoList
+	# attr_reader : 
 
-	def initialize(string)
-		todo_list = []
-		todo_list << string.split(',')
-		return todo_list
+	# def initialize(string)
+	# 	todo_list = string.split(',')
+	# 	return todo_list
+	# end
+
+	def initialize(task)
+		@list = []
+		@list = task
+		# @list << task[1]
+		# @list
 	end
 
-	def gets_method
+	def get_items()
+		@list 
+	end
+
+	def add_item(item)
+		@list << item
 
 	end
 
+	def delete_item(input)
+		@list.delete(input)
+	end
 
-
+	def get_item(int)
+		@list[int]
+	end
 end
 
-p TodoList.new("do the dishes", "mow the lawn")
+# p TodoList.new("do the dishes, mow the lawn")
+# p TodoList.new(["do the dishes", "mow the lawn"])
