970bf8c472e985d906b529c41f7fb961ca021de0
Karan Shetty <kshetty614@gmail.com>
2016-12-08 16:03:23 -0500

    Add some pseudocode to explore.js.

---
+++ js/explore.js
@@ -0,0 +1,7 @@
+// psuedocode
+
+// split string into an array
+// iterate through original array, adding letters at the beginning of a new array
+// new array should contain letters in reverse order
+// join new array and output to console. 
+
