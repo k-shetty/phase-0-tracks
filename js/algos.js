39cf1b2c8e0b3241181f12807b54a4a60aff1a8d
Karan Shetty <kshetty614@gmail.com>
2016-12-11 16:20:40 -0500

    Create algos.js. Add pseudocode for release 0.

---
+++ js/algos.js
@@ -0,0 +1,10 @@
+// Release 0
+//write a function that takes an array of words or phrases and returns the longest word or phrase in the array. 
+//So if we gave your function the array of ["long phrase","longest phrase","longer phrase"], it would return "longest phrase". 
+
+//Input: array
+//Output: string
+//Create variable with empty string as place holder.
+//Iterate through array, compare length of current index with length of the place holder variable.
+//Phrase with longest count gets assigned to place holder variable.
+
