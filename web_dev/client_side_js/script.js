914886b6e8e13211aff686718bb845ee52d85817
Karan Shetty <kshetty614@gmail.com>
2016-12-26 16:47:17 -0500

    Add code for release 2 in script.js.

--- web_dev/client_side_js/script.js
+++ web_dev/client_side_js/script.js
@@ -7,3 +7,9 @@ var div = document.querySelectorAll('div');
 div.forEach(function(element) {
 	element.style.border = "2px dotted green";
 });
+
+div.forEach(function(element){
+	element.addEventListener("mouseover", function() {
+		element.style.color = "red";
+	});
+});
