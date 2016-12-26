7607a88d71489f8e5476c2681bf8b9db7c53c011
Karan Shetty <kshetty614@gmail.com>
2016-12-26 16:09:45 -0500

    Add release 1 code to script.js.

--- web_dev/client_side_js/script.js
+++ web_dev/client_side_js/script.js
@@ -1,1 +1,9 @@
 console.log("The script is running!");
+
+// var div = document.getElementsByTagName('div');
+
+var div = document.querySelectorAll('div');
+
+div.forEach(function(element) {
+	element.style.border = "2px dotted green";
+});
